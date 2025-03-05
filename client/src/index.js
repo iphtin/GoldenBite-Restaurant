497ab1a260823875cfa9b1d38b0276645120a6e3
abdinasir <abdinasiriphtin@gmail.com>
2025-03-05 12:20:07 -0800

    updated index.html

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-HZYC422CCK");
+ReactGA.send("pageview");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
